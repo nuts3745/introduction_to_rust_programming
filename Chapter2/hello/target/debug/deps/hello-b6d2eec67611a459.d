9a20eeab1db1ddb881ed92937f80515e64957e57
nuts3745 <kohji_suzuki@me.com>
2020-08-28 00:47:08 +0900

    チャプター2で環境構築した

---
+++ Chapter2/hello/target/debug/deps/hello-b6d2eec67611a459.d
@@ -0,0 +1,5 @@
+/home/nuts3745/rust_prac/Chapter2/hello/target/debug/deps/hello-b6d2eec67611a459: src/main.rs
+
+/home/nuts3745/rust_prac/Chapter2/hello/target/debug/deps/hello-b6d2eec67611a459.d: src/main.rs
+
+src/main.rs:
---
+++ Chapter2/hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+/home/nuts3745/rust_prac/Chapter2/hello/target/debug/hello: /home/nuts3745/rust_prac/Chapter2/hello/src/main.rs
