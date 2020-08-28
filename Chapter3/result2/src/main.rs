dfd1e3ea591ac7a2e04eaef493c9d265c4a34c96
nuts3745 <kohji_suzuki@me.com>
2020-08-28 13:41:21 +0900

    一応再利用できた P44

--- Chapter3/result2/src/main.rs
+++ Chapter3/result2/src/main.rs
@@ -5,7 +5,7 @@ fn main() {
         println!("code: {}", code);
     }
 
-    let result: Result<i32, String> = Ok(200);
+    //let result: Result<i32, String> = Ok(200);
     println!("code: {}", result.unwrap_or(-1));
     let result: Result<i32, String> = Err("error".to_string());
     println!("code: {}", result.unwrap_or(-1));
