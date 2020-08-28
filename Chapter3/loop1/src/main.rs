2c4b968f539d0e1030aa00c4ec32e48f14c77938
nuts3745 <kohji_suzuki@me.com>
2020-08-28 18:12:02 +0900

    3種のループについて

--- Chapter3/loop1/src/main.rs
+++ Chapter3/loop1/src/main.rs
@@ -1,6 +1,8 @@
 fn main() {
     let mut count = 0;
 
+    println!("\n loop{} \n", "");
+
     let _result = loop {
         println!("count: {}", count);
         count += 1;
@@ -9,11 +11,28 @@ fn main() {
         }
     };
 
-    println!("{}", "");
+    println!("\n while{} \n", "");
+
     let mut count = 0;
 
     while count < 10 {
         println!("count: {}", count);
         count += 1;
+    }
+
+    println!("\n for1{} \n", "");
+
+    let _count: i32;
+
+    for count in 0..10 {
+        println!("count: {}", count);
+    }
+
+    println!("\n for2{} \n", "");
+
+    let array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    
+    for element in &array {
+        println!("element: {}", element);
     }
 }
