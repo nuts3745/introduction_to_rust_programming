b112cf9c29d146fea579fef05720a8b014265956
nuts3745 <kohji_suzuki@me.com>
2020-08-28 14:34:48 +0900

    for文の使い方がわかった

--- Chapter3/vec/src/main.rs
+++ Chapter3/vec/src/main.rs
@@ -1,5 +1,8 @@
 fn main() {
     let v1 = vec![1, 2, 3, 4, 5];
     let v2 = vec![0; 5];
-    println!("{}", v1[0]);
+    println!("{}", v2[0]);
+    for element in &v1 {
+        println!("{}", element);
+    }
 }
