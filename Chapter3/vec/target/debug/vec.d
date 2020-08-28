087f2df507b0d9631b5ac0638efb9a8e29ee645a
nuts3745 <kohji_suzuki@me.com>
2020-08-28 14:29:59 +0900

    定義した変数を出力する方法がわからん P46

---
+++ Chapter3/vec/target/debug/vec.d
@@ -0,0 +1,1 @@
+/home/nuts3745/rust_prac/Chapter3/vec/target/debug/vec: /home/nuts3745/rust_prac/Chapter3/vec/src/main.rs
---
+++ Chapter3/vec/target/rls/debug/deps/vec-937b2edd78fa1e48.d
@@ -0,0 +1,5 @@
+/home/nuts3745/rust_prac/Chapter3/vec/target/rls/debug/deps/vec-937b2edd78fa1e48.rmeta: src/main.rs
+
+/home/nuts3745/rust_prac/Chapter3/vec/target/rls/debug/deps/vec-937b2edd78fa1e48.d: src/main.rs
+
+src/main.rs:
