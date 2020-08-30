6f12840907c706a965f92d90db5120cfaf0dd556
nuts3745 <kohji_suzuki@me.com>
2020-08-30 21:23:18 +0900

    deriveを利用することで自作の型にトレイトを導出できるらしい P68

---
+++ Chapter3/trait1/target/debug/trait1.d
@@ -0,0 +1,1 @@
+/home/nuts3745/rust_prac/Chapter3/trait1/target/debug/trait1: /home/nuts3745/rust_prac/Chapter3/trait1/src/main.rs
---
+++ Chapter3/trait1/target/rls/debug/deps/trait1-708aac47de431b96.d
@@ -0,0 +1,5 @@
+/home/nuts3745/rust_prac/Chapter3/trait1/target/rls/debug/deps/trait1-708aac47de431b96.rmeta: src/main.rs
+
+/home/nuts3745/rust_prac/Chapter3/trait1/target/rls/debug/deps/trait1-708aac47de431b96.d: src/main.rs
+
+src/main.rs:
