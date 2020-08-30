0bd9549e2e1252d6f0c1d3f13f4b789a7f434e62
nuts3745 <kohji_suzuki@me.com>
2020-08-30 22:23:37 +0900

    参照で渡して借用する方法

--- Chapter3/borrow1/src/main.rs
+++ Chapter3/borrow1/src/main.rs
@@ -1,12 +1,11 @@
 fn main() {
-    let mut important_data = "Hello, World!".to_string();
+    let important_data = "Hello, World!".to_string();
 
-    important_data = calc_data(important_data);
+    calc_data(&important_data);
 
-    println!("{}", important_data);
+    println!("{}", &important_data);
 }
 
-fn calc_data(data: String) -> String {
+fn calc_data(data: &String) {
     println!("{}", data);
-    data
 }
