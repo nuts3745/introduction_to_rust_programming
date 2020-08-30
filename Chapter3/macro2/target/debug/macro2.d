8c38a9d8d55a2ba9a6642d611f17e2adac64fe9b
nuts3745 <kohji_suzuki@me.com>
2020-08-30 17:56:28 +0900

    データ出力用のマクロ

---
+++ Chapter3/macro2/target/debug/macro2.d
@@ -0,0 +1,1 @@
+/home/nuts3745/rust_prac/Chapter3/macro2/target/debug/macro2: /home/nuts3745/rust_prac/Chapter3/macro2/src/main.rs
---
+++ Chapter3/macro2/target/rls/debug/deps/macro2-d9c0adf76dca928f.d
@@ -0,0 +1,5 @@
+/home/nuts3745/rust_prac/Chapter3/macro2/target/rls/debug/deps/macro2-d9c0adf76dca928f.rmeta: src/main.rs
+
+/home/nuts3745/rust_prac/Chapter3/macro2/target/rls/debug/deps/macro2-d9c0adf76dca928f.d: src/main.rs
+
+src/main.rs:
