cb1a50ca9c140b31b85360269a255a74e51c0370
nuts3745 <kohji_suzuki@me.com>
2020-08-30 22:41:21 +0900

    所有者のライフタイムを超えて借用者が参照することはできない

---
+++ Chapter3/borrow3/target/debug/deps/borrow3-c5b0ffe86e9b58ac.d
@@ -0,0 +1,5 @@
+/home/nuts3745/rust_prac/Chapter3/borrow3/target/debug/deps/borrow3-c5b0ffe86e9b58ac: src/main.rs
+
+/home/nuts3745/rust_prac/Chapter3/borrow3/target/debug/deps/borrow3-c5b0ffe86e9b58ac.d: src/main.rs
+
+src/main.rs:
