5324283cebf828144b4d61bfdc0b0cae75bedc76
nuts3745 <kohji_suzuki@me.com>
2020-08-30 17:57:35 +0900

    異常終了用のマクロ

---
+++ Chapter3/macro3/target/debug/macro3.d
@@ -0,0 +1,1 @@
+/home/nuts3745/rust_prac/Chapter3/macro3/target/debug/macro3: /home/nuts3745/rust_prac/Chapter3/macro3/src/main.rs
---
+++ Chapter3/macro3/target/rls/debug/deps/macro3-12c2d99d7caf0783.d
@@ -0,0 +1,5 @@
+/home/nuts3745/rust_prac/Chapter3/macro3/target/rls/debug/deps/macro3-12c2d99d7caf0783.rmeta: src/main.rs
+
+/home/nuts3745/rust_prac/Chapter3/macro3/target/rls/debug/deps/macro3-12c2d99d7caf0783.d: src/main.rs
+
+src/main.rs:
