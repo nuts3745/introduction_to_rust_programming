b947d0be831f6fa613db7e1ec208e04af3c37510
nuts3745 <kohji_suzuki@me.com>
2020-08-30 22:27:41 +0900

    不変な参照は複数に渡せる

---
+++ Chapter3/borrow2/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let x = 5;
+    let y = &x;
+    let z = &x;
+
+    dbg!(x);
+    dbg!(y);
+    dbg!(z);
+}
---
+++ Chapter3/borrow2/target/debug/borrow2.d
@@ -0,0 +1,1 @@
+/home/nuts3745/rust_prac/Chapter3/borrow2/target/debug/borrow2: /home/nuts3745/rust_prac/Chapter3/borrow2/src/main.rs
