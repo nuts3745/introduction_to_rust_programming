2631e3cb0d2cbd6935dad95d2be141d3b825428f
nuts3745 <kohji_suzuki@me.com>
2020-08-31 14:50:13 +0900

    共有メモリを用意したが、所有権が共有できていない

--- Chapter3/thread1/src/main.rs
+++ Chapter3/thread1/src/main.rs
@@ -2,14 +2,16 @@ use std::thread;
 
 fn main() {
     let mut handles = Vec::new();
+    let mut data = vec![1; 10];
 
     for x in 0..10 {
-        handles.push(thread::spawn(|| {
-            println!("Hello, world!: {}", x);
+        handles.push(thread::spawn(move || {
+            data[x] += 1;
         }));
     }
 
     for handle in handles {
         let _ = handle.join();
     }
+    dbg!(data);
 }
