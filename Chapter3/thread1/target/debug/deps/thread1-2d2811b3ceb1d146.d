b3f338992d26275776c457e8f6d5a944582f74a7
nuts3745 <kohji_suzuki@me.com>
2020-08-31 14:37:23 +0900

    スレッドを作ったが出力される前にプログラムが終了している

---
+++ Chapter3/thread1/target/debug/deps/thread1-2d2811b3ceb1d146.d
@@ -0,0 +1,5 @@
+/home/nuts3745/rust_prac/Chapter3/thread1/target/debug/deps/thread1-2d2811b3ceb1d146: src/main.rs
+
+/home/nuts3745/rust_prac/Chapter3/thread1/target/debug/deps/thread1-2d2811b3ceb1d146.d: src/main.rs
+
+src/main.rs:
---
+++ Chapter3/thread1/target/debug/thread1.d
@@ -0,0 +1,1 @@
+/home/nuts3745/rust_prac/Chapter3/thread1/target/debug/thread1: /home/nuts3745/rust_prac/Chapter3/thread1/src/main.rs
