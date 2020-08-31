15c306369ac1a3d83d7629f8a4edab3e38d5298b
nuts3745 <kohji_suzuki@me.com>
2020-09-01 00:29:36 +0900

    asyncブロックを使った方法も追記

--- Chapter3/async3/src/main.rs
+++ Chapter3/async3/src/main.rs
@@ -1,4 +1,13 @@
 use futures::executor;
+/*
+fn something_great_async_function() -> impl Future<Output = i32> {
+    async {
+        let ans = async_add(2, 3).await;
+        println!("{}", ans);
+        ans
+    }
+}
+*/
 
 fn main() {
     async fn async_add(left: i32, right: i32) -> i32 {
