dc64628402aa25e2c0d6476cdbe52602381cb1dc
nuts3745 <japanbik@gmail.com>
2021-01-19 12:25:26 +0900

    clapのderiveマクロで入出力を簡潔にした。型をOptionにするか否かで必須項目かどうかを指定する。Optionにしなくても、default_valueを指定しておけばOK

--- Chapter4/samplecli/src/main.rs
+++ Chapter4/samplecli/src/main.rs
@@ -1,30 +1,33 @@
-use clap::{App, Arg};
+use clap::Clap;
+
+#[derive(Clap, Debug)]
+#[clap(
+    name = "My RPN program",
+    version = "1.0.0",
+    author = "Your name",
+    about = "Super awesome sample RPN calculator"
+)]
+
+struct Opts {
+    #[clap(short, long)]
+    verbose: bool,
+
+    #[clap(name = "NUMBER")]
+    num: Option<i32>,
+
+    #[clap(name = "FILE", default_value = "default.txt")]
+    formula_file: String,
+}
 
 fn main() {
-    let matches = App::new("My RPN program")
-    .version("1.0.0")
-    .author("Your name")
-    .about("Super awesome sample RPN calculator")
-    .arg(
-        Arg::new("formula_file")
-        .about("Formulas written in RPN")
-        .value_name("FILE")
-        .index(1)
-        .required(false),
-    )
-    .arg(
-        Arg::new("verbose")
-        .about("Sets the level of verbosity")
-        .short('v')
-        .long("verbose")
-        .required(false),
-    )
-    .get_matches();
+    let opts = Opts::parse();
 
-    match matches.value_of("formula_file") {
-        Some(file) => println!("File specified: {}", file),
-        None => println!("No file specified."),
+    match opts.num {
+        Some(num) => println!("Your number: {}", num),
+        None => println!("No number specified."),
     }
-    let verbose = matches.is_present("verbose");
-    println!("Is verbosity specified?: {}", verbose);
+
+     println!("File specified: {}", opts.formula_file);
+
+    println!("Is verbosity specified?: {}", opts.verbose);
 }
