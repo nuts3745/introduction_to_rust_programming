4038bbec78bf586034b481fed026ffa073d7bdae
nuts3745 <japanbik@gmail.com>
2021-01-21 19:58:01 +0900

    独自のエラー型を作ってエラーハンドリングした

--- Chapter4/samplecli/src/bin/err_panic.rs
+++ Chapter4/samplecli/src/bin/err_panic.rs
@@ -1,16 +1,39 @@
-fn get_int_from_file() -> i32 {
+use std::fmt;
+
+enum MyError {
+  Io(std::io::Error),
+  Num(std::num::ParseIntError),
+}
+
+impl fmt::Display for MyError {
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    match self {
+      MyError::Io(cause) => write!(f, "I/O Error: {}", cause),
+      MyError::Num(cause) => write!(f, "Parse Error: {}", cause),
+    }
+  }
+}
+
+impl From<std::io::Error> for MyError {
+  fn from(cause: std::io::Error) -> Self {
+    Self::Io(cause)
+  }
+}
+
+fn get_int_from_file() -> Result<i32, MyError> {
   let path = "number.txt";
+  let num_str = std::fs::read_to_string(path).map_err(MyError::from)?;
 
-  let num_str = std::fs::read_to_string(path).expect("failed to open the file.");
-
-  let ret = num_str
+  num_str
     .trim()
     .parse::<i32>()
-    .expect("failed to parse string to a number.");
-  
-  ret * 2
+    .map(|t| t * 2)
+    .map_err(|e| MyError::Num(e))
 }
 
 fn main() {
-  println!("{}", get_int_from_file());
+  match get_int_from_file() {
+    Ok(x) => println!("{}", x),
+    Err(e) => println!("{}", e),
+  }
 }
