e6b9142298089199c2afa3a59816180bf047dfd1
nuts3745 <japanbik@gmail.com>
2021-01-22 12:56:30 +0900

    dockerイメージにビルドできるようにした。ローカルではビルドにめちゃくちゃ時間がかかるので途中で中断したので動作未確認。

--- Chapter5/todo/src/main.rs
+++ Chapter5/todo/src/main.rs
@@ -1,6 +1,9 @@
-use actix_web::{get, App, HttpResponse, HttpServer, ResponseError};
+use actix_web::{get, post, http::header, web, App, HttpResponse, HttpServer, ResponseError};
 use thiserror::Error;
 use askama::Template;
+use r2d2::Pool;
+use r2d2_sqlite::{SqliteConnectionManager, rusqlite};
+use serde::Deserialize;
 
 struct TodoEntry {
     id: u32,
@@ -18,23 +21,40 @@ struct IndexTemplate {
 enum MyError {
     #[error("Failed to render HTML")]
     AskamaError(#[from] askama::Error),
+
+    #[error("Failed to get connection")]
+    ConnectionPoolError(#[from] r2d2::Error),
+
+    #[error("Failed SQL execution")]
+    SQLiteError(#[from] rusqlite::Error),
 }
 
 impl ResponseError for MyError {}
 
-#[get("/")]
-async fn index() -> Result<HttpResponse, MyError> {
-    let mut entries = Vec::new();
+#[derive(Deserialize)]
+struct AddParams {
+    text: String,
+}
 
-    entries.push(TodoEntry{
-        id: 1,
-        text: "First entry".to_string(),
-    });
-    entries.push(TodoEntry{
-        id: 2,
-        text: "Second entry".to_string(),
-    });
-    
+#[derive(Deserialize)]
+struct DeleteParams {
+    id: u32,
+}
+
+#[get("/")]
+async fn index(db: web::Data<Pool<SqliteConnectionManager>>) -> Result<HttpResponse, MyError> {
+    let conn = db.get()?;
+    let mut statement = conn.prepare("SELECT id, text FROM todo")?;
+    let rows = statement.query_map(rusqlite::params![], |row| {
+        let id = row.get(0)?;
+        let text = row.get(1)?;
+        Ok(TodoEntry { id, text })
+    })?;
+
+    let mut entries = Vec::new();
+    for row in rows {
+        entries.push(row?);
+    }    
     let html = IndexTemplate { entries };
     let response_body = html.render()?;
     Ok(HttpResponse::Ok()
@@ -43,9 +63,47 @@ async fn index() -> Result<HttpResponse, MyError> {
     )
 }
 
+#[post("/add")]
+async fn add_todo(
+    params: web::Form<AddParams>,
+    db: web::Data<r2d2::Pool<SqliteConnectionManager>>,
+) -> Result<HttpResponse, MyError> {
+    let conn = db.get()?;
+    conn.execute("INSERT INTO todo (text) VALUES (?)", &[&params.text])?;
+    Ok(HttpResponse::SeeOther()
+        .header(header::LOCATION, "/")
+        .finish())
+}
+
+#[post("/delete")]
+async fn delete_todo(
+    params: web::Form<DeleteParams>,
+    db: web::Data<r2d2::Pool<SqliteConnectionManager>>,
+) -> Result<HttpResponse, MyError> {
+    let conn = db.get()?;
+    conn.execute("DELETE FROM todo WHERE id=?", &[params.id])?;
+    Ok(HttpResponse::SeeOther()
+        .header(header::LOCATION, "/")
+        .finish())
+}
+
+
 #[actix_rt::main]
 async fn main() -> Result<(), actix_web::Error> {
-    HttpServer::new(move || App::new().service(index))
+    let manager = SqliteConnectionManager::file("todo.db");
+    let pool = Pool::new(manager).expect("Failed to initialize the connection pool.");
+    let conn = pool
+        .get()
+        .expect("Failed to get the connection from the pool.");
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS todo (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            text TEXT NOT NULL
+        )",
+        rusqlite::params![],
+    )
+    .expect("Failed to create a table `todo`.");
+    HttpServer::new(move || App::new().service(index).service(add_todo).service(delete_todo).data(pool.clone()))
         .bind("0.0.0.0:8080")?
         .run()
         .await?;
